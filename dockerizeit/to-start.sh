1d0abf985f0aec7915b1a8418b16adf0257805c5
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2016-04-20 06:34:33 +0200

    Fix #18: remove useless scritps
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

--- dockerizeit/to-start.sh
+++
@@ -1,3 +0,0 @@
-docker-compose -p project build
-docker-compose -p project up -d
-docker-compose -p project scale jslave=3
--- dockerizeit/to-stop.sh
+++
@@ -1,4 +0,0 @@
-docker-compose -p project stop
-docker-compose -p project rm -f
-
-
