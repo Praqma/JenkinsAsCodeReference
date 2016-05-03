6e75b627aa455cb8a5e53d8a0923d72193abeb0b
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2016-05-03 16:16:43 +0200

    Fix #37: Add check that proxy is set before trying to convert it to URL
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

--- dockerizeit/master/proxy.groovy
+++ dockerizeit/master/proxy.groovy
@@ -1,8 +1,8 @@
 import hudson.model.*
 import jenkins.*
 import jenkins.model.*
-import hudson.slaves.EnvironmentVariablesNodeProperty;
-import hudson.slaves.EnvironmentVariablesNodeProperty.Entry;
+import hudson.slaves.EnvironmentVariablesNodeProperty
+import hudson.slaves.EnvironmentVariablesNodeProperty.Entry
 
 println "--> Setting up proxy"
 
@@ -10,6 +10,20 @@ def gradle_opts = ""
 def proxy_vars = ['HTTP_PROXY', 'HTTPS_PROXY', 'NO_PROXY']
 for (e in System.getenv()) {
   def key = e.key.toUpperCase()
+  if ( ! proxy_vars.contains(key) ) {
+    continue
+  }
+  if ( e.value == "" ) {
+    println "${e.key} defined but contains empty string. Skip it"
+    continue
+  }
+  // Add proxy variables to the Jenkins global config - they are already available as env variables set
+  // by Docker build but we want to increase visibility for the users
+  EnvironmentVariablesNodeProperty.Entry entry = new EnvironmentVariablesNodeProperty.Entry(e.key, e.value)
+  Jenkins.instance.getGlobalNodeProperties().add(new EnvironmentVariablesNodeProperty(entry))
+  Jenkins.instance.save()
+  println "Added global environment variable ${e.key}=${e.value}"
+  // Prepare GRADLE_OPTS variable
   switch (e.key.toUpperCase()) {
     case 'NO_PROXY':
       println "No proxy configuration found ${e.key} = ${e.value}"
@@ -30,8 +44,12 @@ for (e in System.getenv()) {
   }
 }
 
-println "Setting up GRADLE_OPTS=${gradle_opts}"
-
-EnvironmentVariablesNodeProperty.Entry entry = new EnvironmentVariablesNodeProperty.Entry("GRADLE_OPTS",gradle_opts);
-Jenkins.instance.getGlobalNodeProperties().add(new EnvironmentVariablesNodeProperty(entry));
-Jenkins.instance.save()
+// Set GRADLE_OPTS variable to configure proxy for Gradle since it does not respect http_proxy
+if ( gradle_opts != "" ) {
+  println "Setting up GRADLE_OPTS=${gradle_opts}"
+  EnvironmentVariablesNodeProperty.Entry entry = new EnvironmentVariablesNodeProperty.Entry("GRADLE_OPTS",gradle_opts);
+  Jenkins.instance.getGlobalNodeProperties().add(new EnvironmentVariablesNodeProperty(entry));
+  Jenkins.instance.save()
+} else {
+  println "GRADLE_OPTS is empty - no proxy settings to set. Do nothing"
+}
