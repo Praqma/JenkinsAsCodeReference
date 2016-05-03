c81b9b16828ec09fe08a6583cd768ea225e72519
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2016-05-03 10:05:44 +0200

    Fix typo introduced during fixing #3
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

--- jobdsl-gradle/src/jobs/resources/pipelines/jenkinsdeploy.groovy
+++ jobdsl-gradle/src/jobs/resources/pipelines/jenkinsdeploy.groovy
@@ -12,7 +12,7 @@ node {
    sh 'cd jobdsl-gradle && ./gradlew test'
 
    stage 'Build master Docker image'
-   sh 'cd dockerizeit/master && docker build --build_arg jenkins_image_version=localhost:5000/reference/jmaster:$(git describe --tags) --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy -t localhost:5000/reference/jmaster:$(git describe --tags) .'
+   sh 'cd dockerizeit/master && docker build --build-arg jenkins_image_version=localhost:5000/reference/jmaster:$(git describe --tags) --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy -t localhost:5000/reference/jmaster:$(git describe --tags) .'
    sh 'docker tag localhost:5000/reference/jmaster:$(git describe --tags) localhost:5000/reference/jmaster:latest'
 
    stage 'Build slave Docker image'
