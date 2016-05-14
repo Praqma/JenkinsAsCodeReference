d35a6b0389885ff890b778c13a10be8796a84e0d
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2016-05-14 15:12:35 +0200

    Fix #56: generate-compose.py doesn't resolve $HOME for registry
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

--- dockerizeit/generate-compose.py
+++ dockerizeit/generate-compose.py
@@ -26,54 +26,71 @@ import logging
 import yaml
 import re
 
-arguments = docopt(__doc__)
+def get_binds(service_name):
+  logging.info("Read {} container config...".format(service_name))
+  command = ["docker", "ps", "-a", "-q", "--no-trunc", "-f", "name=dockerizeit_{}_".format(service_name)]
+  logging.debug("Get {} container id: {}".format(service_name, " ".join(command)))
+  container_id = check_output(command).strip()
+  logging.debug("{} container id: {}".format(service_name, container_id))
+  if bool(re.match("[A-Za-z0-9]{65}", container_id)):
+    raise ValueError("Unexpected result. Expected docker container id, got {}. Command: {}".format(container_id, " ".join(command)))
+  binds = check_output(["docker", "inspect", '--format="{{ .HostConfig.Binds }}"', container_id]).strip()
+  logging.debug("{} binds: {}".format(service_name, binds))
+  return binds
 
-if arguments['--debug']:
-  logging.basicConfig(level=logging.DEBUG)
+def main():
+  arguments = docopt(__doc__)
 
-logging.debug("Arguments: {}".format(arguments))
+  if arguments['--debug']:
+    logging.basicConfig(level=logging.DEBUG)
 
-with open(arguments['--file'], 'r') as f:
-  doc = yaml.load(f)
+  logging.debug("Arguments: {}".format(arguments))
 
-logging.debug("Provide docker compose file: {}".format(doc))
+  with open(arguments['--file'], 'r') as f:
+    doc = yaml.load(f)
 
-logging.info("Read jmaster container config...")
-# TODO: add exceptions handling in case we need sudo to run docker
-jmaster_id=check_output(["docker", "ps", "-a", "-q", "-f", "name=jmaster"]).strip()
-logging.debug("jmaster container id: {}".format(jmaster_id))
-# TODO: check that we got one id - not two and not none
-jmaster_binds=check_output(["docker", "inspect", '--format="{{ .HostConfig.Binds }}"', jmaster_id]).strip()
-logging.debug("jmaster binds: {}".format(jmaster_binds))
-logging.info("Read jslave container config...")
-jslave_id=check_output(["docker", "ps", "-a", "-q", "-f", "name=jslave"]).strip()
-logging.debug("jslave container id: {}".format(jslave_id))
-# TODO: check that we got one id - not two and not none
-jslave_binds=check_output(["docker", "inspect", '--format="{{ .HostConfig.Binds }}"', jslave_id]).strip()
-logging.debug("jslave binds: {}".format(jslave_binds))
+  logging.debug("Provided docker compose file: {}".format(doc))
 
-logging.info("Update definition for jmaster. Relace build with image...")
-logging.debug("Before: {}".format(doc["services"]["jmaster"]))
-del doc["services"]["jmaster"]["build"]
-doc["services"]["jmaster"]["image"] = "{}:{}".format(arguments['--jmaster-image'], arguments['--jmaster-version'])
-doc["services"]["jmaster"]["volumes"] = jmaster_binds.strip('[|]').replace(":rw","").split()
-# We have to check that we don't have not mounted volumes like 1ab3ba428445786de381d741cd2d3c4dff2e956342f02712ef205fa63ba47779:/var/jenkins_home
-# This volume comes from Jenkins Dockerfile - it is declared there but we do not mount it explicitly
-# If bring it to the compose file like this 1ab3ba428445786de381d741cd2d3c4dff2e956342f02712ef205fa63ba47779:/var/jenkins_home then
-# docker-compose won't be able to create new container to replace old one since we have direct reference to the volume mount point
-pattern=re.compile("^[a-zA-Z0-9]+:/var/jenkins_home$")
-for item in doc["services"]["jmaster"]["volumes"]:
-  if re.findall(pattern, item):
-    logging.debug("Found not mounted volume {}. Drop it from the volumes list...".format(re.findall(pattern, item)[0]))
-    doc["services"]["jmaster"]["volumes"].remove(re.findall(pattern, item)[0])
-logging.debug("After: {}".format(doc["services"]["jmaster"]))
+  # Update jmaster
+  logging.info("Update definition for jmaster. Relace build with image...")
+  logging.debug("Before: {}".format(doc["services"]["jmaster"]))
+  del doc["services"]["jmaster"]["build"]
+  doc["services"]["jmaster"]["image"] = "{}:{}".format(arguments['--jmaster-image'], arguments['--jmaster-version'])
 
-logging.info("Update definition for jslave. Relace build with image...")
-logging.debug("Before: {}".format(doc["services"]["jslave"]))
-del doc["services"]["jslave"]["build"]
-doc["services"]["jslave"]["image"] = "{}:{}".format(arguments['--jslave-image'], arguments['--jslave-version'])
-doc["services"]["jslave"]["volumes"] =jslave_binds.strip('[|]').replace(":rw","").split()
-logging.debug("After: {}".format(doc["services"]["jslave"]))
+  # We are resolving binds to make sure that we point out correct location of the home directory
+  # Use case: starting containers using docker-machine - on the start path resolved to the home directory on the host
+  # When munchausen restarts services he will be inside Linux virtual machine and home directory will be resolved differently
+  logging.info("Update definition for jmaster. Resolve binds...")
+  jmaster_binds = get_binds("jmaster")
+  doc["services"]["jmaster"]["volumes"] = jmaster_binds.strip('[|]').replace(":rw","").split()
+  # We have to check that we don't have not mounted volumes like 1ab3ba428445786de381d741cd2d3c4dff2e956342f02712ef205fa63ba47779:/var/jenkins_home
+  # This volume comes from Jenkins Dockerfile - it is declared there but we do not mount it explicitly
+  # If bring it to the compose file like this 1ab3ba428445786de381d741cd2d3c4dff2e956342f02712ef205fa63ba47779:/var/jenkins_home then
+  # docker-compose won't be able to create new container to replace old one since we have direct reference to the volume mount point
+  pattern=re.compile("^[a-zA-Z0-9]+:/var/jenkins_home$")
+  for item in doc["services"]["jmaster"]["volumes"]:
+    if re.findall(pattern, item):
+      logging.debug("Found not mounted volume {}. Drop it from the volumes list...".format(re.findall(pattern, item)[0]))
+      doc["services"]["jmaster"]["volumes"].remove(re.findall(pattern, item)[0])
+  logging.debug("After: {}".format(doc["services"]["jmaster"]))
 
-with open('docker-compose.yml', 'w') as outfile:
+  # Update jslave
+  logging.info("Update definition for jslave. Relace build with image...")
+  logging.debug("Before: {}".format(doc["services"]["jslave"]))
+  del doc["services"]["jslave"]["build"]
+  doc["services"]["jslave"]["image"] = "{}:{}".format(arguments['--jslave-image'], arguments['--jslave-version'])
+  jslave_binds = get_binds("jslave")
+  doc["services"]["jslave"]["volumes"] =jslave_binds.strip('[|]').replace(":rw","").split()
+
+  # Registry might be removed and replaced by something else so we have to check that it is there
+  if "registry" in doc["services"]:
+    logging.debug("Before: {}".format(doc["services"]["registry"]))
+    registry_binds = get_binds("registry")
+    doc["services"]["registry"]["volumes"] = registry_binds.strip('[|]').replace(":rw","").split()
+    logging.debug("After: {}".format(doc["services"]["registry"]))
+
+  with open('docker-compose.yml', 'w') as outfile:
     outfile.write( yaml.dump(doc, default_flow_style=False) )
+
+if __name__ == "__main__":
+    main()
