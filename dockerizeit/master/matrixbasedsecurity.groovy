Praqma/JenkinsAsCodeReference

commit d336668b17545a57546ad7a0a63f6863c703d371

Fix #15: Add configuration of Matrix-based security (#27)

dockerizeit/master/matrixbasedsecurity.groovy
@@ -1,0 +1,30 @@
+import java.util.Properties
+import java.lang.System
+import jenkins.model.*
+import hudson.security.*
+import org.jenkinsci.plugins.*
+import hudson.model.*
+
+println "--> Matrix-Based security: Read properties from the file"
+
+Properties properties = new Properties()
+def home_dir = System.getenv("JENKINS_HOME")
+File propertiesFile = new File("$home_dir/jenkins.properties")
+propertiesFile.withInputStream {
+    properties.load(it)
+}
+if(properties.isMatrixBasedSecurity.toBoolean()){
+	println "--> Configure Matrix-Based security"
+
+	def instance = Jenkins.getInstance()
+	def anonymous = hudson.security.ACL.ANONYMOUS_USERNAME
+	def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+	instance.setSecurityRealm(hudsonRealm)
+
+	def strategy = new GlobalMatrixAuthorizationStrategy()
+	strategy.add(Jenkins.READ, anonymous)
+	strategy.add(Jenkins.ADMINISTER, "authenticated")
+	instance.setAuthorizationStrategy(strategy)
+
+	instance.save()
+}

dockerizeit/master/jenkins.properties
@@ -1,5 +1,6 @@
 isArtifactory=true
 isLDAP=true
+isMatrixBasedSecurity=false
 jenkinsSSHUserId=jenkins
 jenkinsUserName=jenkins
 globalConfigname=Jenkins Jenkinsson

dockerizeit/master/securityLDAP.groovy
@@ -4,7 +4,7 @@ import jenkins.model.*
 import hudson.security.*
 import org.jenkinsci.plugins.*
 
-println "--> Read properties from the file"
+println "--> LDAPsecurity: Read properties from the file"
 
 Properties properties = new Properties()
 def home_dir = System.getenv("JENKINS_HOME")

commit 145ebed9602ce9b9e848c9061cb46e52eae585ad

Fix issue introduced in #27 - do not set security realm, only configure matrix
Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

dockerizeit/master/matrixbasedsecurity.groovy
@@ -12,8 +12,6 @@ if(properties.matrixbasedsecurity.enabled){
 
   def instance = Jenkins.getInstance()
   def anonymous = hudson.security.ACL.ANONYMOUS_USERNAME
-  def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-  instance.setSecurityRealm(hudsonRealm)
 
   def strategy = new GlobalMatrixAuthorizationStrategy()
   strategy.add(Jenkins.READ, anonymous)