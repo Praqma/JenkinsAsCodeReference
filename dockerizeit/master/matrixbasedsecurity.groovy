fabdaa5668dae1bce071857c481fc55a77dfa64b
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2016-05-26 17:32:40 +0200

    Update #66: Use single configuration file for the matrixbased secutiry
    - Move users closure to security.properties
    - Add read Job permission to anonymous user
    - Make all authenticated users admin instead of only jenkins
    
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

--- dockerizeit/master/matrixbasedsecurity.groovy
+++ dockerizeit/master/matrixbasedsecurity.groovy
@@ -6,17 +6,18 @@ import hudson.model.*
 
 def home_dir = System.getenv("JENKINS_HOME")
 def properties = new ConfigSlurper().parse(new File("${home_dir}/security.properties").toURI().toURL())
-// To set permissions use list of Permission only from hudson.model.* 
-def userProps = new ConfigSlurper().parse(new File("${home_dir}/permissions.properties").toURI().toURL())
 
 if(properties.matrixbasedsecurity.enabled){
-	println "--> Configure Matrix-Based security"
-	def strategy = new GlobalMatrixAuthorizationStrategy()
+  println "--> Configure Matrix-Based security"
+  def strategy = new GlobalMatrixAuthorizationStrategy()
 
-	userProps.each() { key, value ->
-		value.permissions.each() { strategy.add(it, value.userId)}
-	}
-	
-	Jenkins.instance.setAuthorizationStrategy(strategy)
-	Jenkins.instance.save()
+  properties.matrixbasedsecurity.users.each() { key, value ->
+    value.permissions.each() {
+      println "--> Add permission ${value.userId} for user ${it}"
+      strategy.add(it, value.userId)
+    }
+  }
+
+  Jenkins.instance.setAuthorizationStrategy(strategy)
+  Jenkins.instance.save()
 }
