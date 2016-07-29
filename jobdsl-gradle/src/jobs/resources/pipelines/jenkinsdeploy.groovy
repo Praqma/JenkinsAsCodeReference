db14759eb22be1100356ef37e35c3b34ad2d380f
Bosse Nyström <bon@praqma.net>
2016-07-29 15:04:43 +0200

    Fix #82: Use gradle daemon

--- jobdsl-gradle/src/jobs/resources/pipelines/jenkinsdeploy.groovy
+++ jobdsl-gradle/src/jobs/resources/pipelines/jenkinsdeploy.groovy
@@ -13,15 +13,21 @@ node(nodeName) {
              userRemoteConfigs: [[credentialsId: env.default_credentials, url: env.default_repo]]])
 
    stage 'Verify JobDSL'
-   sh 'cd jobdsl-gradle && ./gradlew buildXml'
-   sh 'cd jobdsl-gradle && ./gradlew test'
+   dir('jobdsl-gradle'){
+     sh './gradlew --daemon buildXml'
+     sh './gradlew --daemon test'
+   }
 
    stage 'Build master Docker image'
-   sh 'cd dockerizeit/master && docker build --build-arg master_image_version=${master_image_name}:$(git describe --tags) --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy --build-arg JAVA_OPTS -t ${master_image_name}:$(git describe --tags) .'
+   dir('dockerizeit/master'){
+     sh 'docker build --build-arg master_image_version=${master_image_name}:$(git describe --tags) --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy --build-arg JAVA_OPTS -t ${master_image_name}:$(git describe --tags) .'
+   }
    sh 'docker tag ${master_image_name}:$(git describe --tags) ${master_image_name}:latest'
 
    stage 'Build slave Docker image'
-   sh 'cd dockerizeit/slave && docker build --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy -t ${slave_image_name}:$(git describe --tags) .'
+   dir('dockerizeit/slave'){
+     sh 'docker build --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy -t ${slave_image_name}:$(git describe --tags) .'
+   }
    sh 'docker tag ${slave_image_name}:$(git describe --tags) ${slave_image_name}:latest'
 
    stage 'Push to registry'
@@ -37,6 +43,8 @@ node(nodeName) {
    stage 'Deploy'
    sh './dockerizeit/generate-compose.py --debug --file dockerizeit/docker-compose.yml --jmaster-image ${master_image_name} --jmaster-version $(git describe --tags) --jslave-image ${slave_image_name} --jslave-version $(git describe --tags)'
    sh 'cp docker-compose.yml dockerizeit/munchausen/'
-   sh 'cd dockerizeit/munchausen && docker build --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy -t munchausen . && docker run -d -v /var/run/docker.sock:/var/run/docker.sock munchausen $(git describe --tags)'
+   dir('dockerizeit/munchausen'){
+     sh 'docker build --build-arg http_proxy --build-arg https_proxy --build-arg no_proxy -t munchausen . && docker run -d -v /var/run/docker.sock:/var/run/docker.sock munchausen $(git describe --tags)'
+   }
    archive 'docker-compose.yml'
 }
