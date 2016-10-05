38caeac4fe01eb36f176b42c560c97b7804dd2ae
DharaniSP <dsp@praqma.com>
2016-10-05 14:15:15 +0200

    Added new slave labels

--- dockerizeit/slave/start.sh
+++ dockerizeit/slave/start.sh
@@ -5,14 +5,14 @@ if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
 
   # jenkins swarm slave
   JAR=`ls -1 /usr/share/jenkins/swarm-client-*.jar | tail -n 1`
-  
+
   PARAMS="-master http://jmaster:8080"
 
   # Set default number of executors (2 by default)
   PARAMS="$PARAMS -executors ${NUM_OF_EXECUTORS:-2}"
 
   # Set labels to slave
-  PARAMS="$PARAMS -labels \"linux\" -labels \"alpine\" -labels \"3.3\" -labels \"java\" -labels \"docker\" -labels \"swarm\" -labels \"utility-slave\""
+  PARAMS="$PARAMS -labels \"linux\" -labels \"alpine\" -labels \"3.3\" -labels \"java\" -labels \"docker\" -labels \"swarm\" -labels \"utility-slave\" -labels \"GiJeLiSlave\" -labels \"linkchecker\""
 
   echo Running java $JAVA_OPTS -jar $JAR -fsroot $HOME/slave $PARAMS "$@"
   exec java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
