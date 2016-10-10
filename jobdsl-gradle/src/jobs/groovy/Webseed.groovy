19a9f3257ee7f5e50e18966d58f4f432f6f72451
DharaniSP <dsp@praqma.com>
2016-10-10 11:30:11 +0200

    Added new build actions

--- jobdsl-gradle/src/jobs/groovy/Webseed.groovy
+++ jobdsl-gradle/src/jobs/groovy/Webseed.groovy
@@ -1,64 +1,56 @@
-def githubUrl = 'https://github.com/Praqma/job-dsl-collection'
-def branchName = "master" //"\${BRANCH}"
-def releasePraqmaCredentials = '100247a2-70f4-4a4e-a9f6-266d139da9db'
-def dockerHostLabel = 'GiJeLiSlave'
-//def project_name = '2git'
-//def repo_name = 'Praqma/2git'
-def cred_id = "100247a2-70f4-4a4e-a9f6-266d139da9db"
-def jobName = "Web_Seed"
-def displayName = "Web_Pipeline"
-def descriptionString = "Web pipeline job"
+  def githubUrl = 'https://github.com/Praqma/job-dsl-collection'
+  def branchName = "master" //"\${BRANCH}"
+  def releasePraqmaCredentials = '100247a2-70f4-4a4e-a9f6-266d139da9db'
+  def dockerHostLabel = 'GiJeLiSlave'
+  def cred_id = '100247a2-70f4-4a4e-a9f6-266d139da9db'
+  def jobName = 'Web_Seed'
+  def descriptionString = 'Web pipeline job'
+  def externalScriptPath = 'web-pipeline-dsl/web_pipeline_dsl.groovy'
 
 
   job(jobName) {
 
-  displayName(displayName)
+  	label(dockerHostLabel)
 
-	label(dockerHostLabel)
-
-  description(descriptionString)
+    description(descriptionString)
 
     triggers {
-      githubPush()
+         githubPush()
     }
 
     logRotator {
-          daysToKeep(90)
+         daysToKeep(90)
     }
 
-    scm {
-      git {
+    environmentVariables {
+        keepBuildVariables(true)
+        keepSystemVariables(true)
+    }
 
-        remote {
-          url(githubUrl)
-          credentials(releasePraqmaCredentials)
+   jdk('(System)')
+
+   scm {
+     git {
+          remote {
+            url(githubUrl)
+            credentials(releasePraqmaCredentials)
+          }
+
+          branch(branchName)
+
+          configure {
+            node ->
+            node / 'extensions' << 'hudson.plugins.git.extensions.impl.CleanBeforeCheckout' {}
+          }
         }
-
-        branch(branchName)
-
-        configure {
-          node ->
-          node / 'extensions' << 'hudson.plugins.git.extensions.impl.CleanBeforeCheckout' {}
-        }
-      }
     }
 
-    steps {
+   steps {
+       dsl {
+           external "$externalScriptPath"
+           removeAction("DELETE")
+           removeViewAction("DELETE")
+           }
+   }
 
-             dsl {
-                 external "web-pipeline-dsl/web_pipeline_dsl.groovy"
-                 removeAction("DELETE")
-                 removeViewAction("DELETE")
-             }
- }
-
-
-    }
-
-
-
-
-
-
-
-//#########################################################################################################
+}
