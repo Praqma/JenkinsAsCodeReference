332844adf785a7836319e40b9cba08765cedf714
DharaniSP <dsp@praqma.com>
2016-11-08 13:49:00 +0100

    updated credentials for local test

--- jobdsl-gradle/src/jobs/groovy/Webseed.groovy
+++ jobdsl-gradle/src/jobs/groovy/Webseed.groovy
@@ -1,8 +1,7 @@
   def githubUrl = 'https://github.com/Praqma/job-dsl-collection'
   def branchName = "master" //"\${BRANCH}"
-  def releasePraqmaCredentials = '100247a2-70f4-4a4e-a9f6-266d139da9db'
+  def releasePraqmaCredentials = 'jenkins'
   def dockerHostLabel = 'GiJeLiSlave'
-  def cred_id = 'jenkins'
   def jobName = 'Web_Seed'
   def descriptionString = 'Web pipeline job'
   def externalScriptPath = 'web-pipeline-dsl/web_pipeline_dsl.groovy'
