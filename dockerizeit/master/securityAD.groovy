24cc5660b38cdae86482f6ad27bcf54698af4431
Andrey Devyatkin <ady@praqma.net>
2017-01-29 21:01:10 +0100

    Fix #131: Update secutiryAD.groovy to support AD plugin 2.0 #135

--- dockerizeit/master/securityAD.groovy
+++ dockerizeit/master/securityAD.groovy
@@ -1,6 +1,7 @@
 import java.lang.System
 import jenkins.model.*
 import hudson.plugins.active_directory.*
+import com.google.common.collect.Lists
 
 def home_dir = System.getenv("JENKINS_HOME")
 def properties = new ConfigSlurper().parse(new File("$home_dir/security.properties").toURI().toURL())
@@ -8,11 +9,15 @@ def properties = new ConfigSlurper().parse(new File("$home_dir/security.properti
 if(properties.ad.enabled) {
   println "--> Configure AD"
   ActiveDirectorySecurityRealm realm = new ActiveDirectorySecurityRealm(properties.ad.domain,
+                                                                        Lists.newArrayList(new ActiveDirectoryDomain(properties.ad.domain, properties.ad.server)),
                                                                         properties.ad.site,
                                                                         properties.ad.bindName,
                                                                         properties.ad.bindPassword,
                                                                         properties.ad.server,
-                                                                        GroupLookupStrategy.valueOf(properties.ad.groupLookupStrategy.toString().toUpperCase()))
+                                                                        GroupLookupStrategy.valueOf(properties.ad.groupLookupStrategy.toString().toUpperCase()),
+									true,
+									true,
+									new CacheConfiguration(1000, 6000))
   Jenkins.instance.setSecurityRealm(realm)
   Jenkins.instance.save()
 }
