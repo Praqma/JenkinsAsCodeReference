4f7f1eb42da3e2a61d54bfdd65749b8ca5313adc
Andrey Devyatkin <ady@praqma.net>
2017-05-27 21:07:13 +0200

    Use legacy remoting mode for update_plugins.sh (#178)

--- dockerizeit/scripts/update_plugins.sh
+++ dockerizeit/scripts/update_plugins.sh
@@ -3,7 +3,7 @@
 set -e
 
 PLUGINS_TXT=$(git rev-parse --show-toplevel)/dockerizeit/master/plugins.txt
-docker exec dockerizeit_jmaster_1 java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ groovy /var/jenkins_home/get_plugins.groovy > ${PLUGINS_TXT}
+docker exec dockerizeit_jmaster_1 java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -remoting -s http://localhost:8080/ groovy /var/jenkins_home/get_plugins.groovy > ${PLUGINS_TXT}
 
 echo "${PLUGINS_TXT} updated. Diff:"
 
