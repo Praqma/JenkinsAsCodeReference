79d5fb06d4c18e98c4cd47bde236a04c922c030b
Andrey Devyatkin <ady@praqma.net>
2017-05-27 18:10:12 +0200

    Fix #169: Added more convenient way to update plugins.txt (#170)

---
+++ dockerizeit/master/scripts/get_plugins.groovy
@@ -0,0 +1,3 @@
+plugins = [:]
+jenkins.model.Jenkins.instance.getPluginManager().getPlugins().each {plugins << ["${it.getShortName()}":"${it.getVersion()}"]}
+plugins.sort().each() { println "${it.key}:${it.value}"}
